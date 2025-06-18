ca54814429375e4b622d399d8cb0757e0c8aa066
AnuragWebSeederTech <srivastava.anurag@webseeder.tech>
2025-06-18 12:31:39 +0530

    removed search bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -72,12 +72,12 @@ const Header = () => {
           </Link>
 
           {/* Search Icon */}
-          <div className="cursor-pointer hover:text-blue-700 transition duration-300 ml-6" onClick={handleSearchToggle}>
+          {/* <div className="cursor-pointer hover:text-blue-700 transition duration-300 ml-6" onClick={handleSearchToggle}>
             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-search">
               <circle cx="11" cy="11" r="8" />
               <path d="m21 21-4.3-4.3" />
             </svg>
-          </div>
+          </div> */}
         </nav>
 
         {/* Search Bar */}
