709ddcdc934a2a9df0cee255a675dd83f9d9113e
WebSeederVinayak <Sharma.vinayak@webseeder.tech>
2025-06-24 02:51:03 +0530

    header changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -26,25 +26,25 @@ const Header = () => {
   return (
     <header className="w-full absolute top-0 left-0 right-0 bg-transparent z-50">
       {/* Top bar */}
-      <div className="text-black bg-transparent text-base px-6 lg:px-10 py-1 mt-3 flex justify-between items-center">
-        <div className="flex block items-center ml-30 space-x-4">
-          <img src="/images/resoundifyLogo1.png" alt="Resoundify Logo" className="h-14 w-auto object-cover rounded-lg" />
+      <div className="text-black bg-white text-base px-6 lg:px-5 py-1 pt-2 flex justify-between items-center">
+        <div className="flex block items-center space-x-4">
+          <img src="/images/resoundifyLogo.jpeg" alt="Resoundify Logo" className="h-14 w-auto object-cover rounded-lg" />
         </div>
-        <div className="flex items-center space-x-6 font-medium tracking-wide">
-          <div className="flex items-center space-x-2 hover:text-white transition duration-300 cursor-pointer">
+        <div className="flex items-center space-x-10 font tracking-wide">
+          <div className="flex items-center space-x-2 hover:text-blue-700 transition duration-300 cursor-pointer">
             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-mail">
               <rect width="20" height="16" x="2" y="4" rx="2" />
               <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
             </svg>
-            <span>resoundify@example.com</span>
+            
           </div>
-          <div className="flex items-center space-x-2 hover:text-white transition duration-300 cursor-pointer">
+          <div className="flex items-center space-x-2 hover:text-blue-700 transition duration-300 cursor-pointer">
             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-phone">
               <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.63A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" />
             </svg>
-            <span>+91-9876543210</span>
+            
           </div>
-          <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" className="hover:text-white transition duration-300">
+          <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" className="hover:text-blue-700 transition duration-300">
             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-linkedin">
               <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
               <rect width="4" height="12" x="2" y="9" />
@@ -55,13 +55,13 @@ const Header = () => {
       </div>
 
       {/* Main nav */}
-      <div className="flex bg-transparent justify-between items-center px-6 lg:px-10 py-1 mx-auto relative">
+      <div className="flex bg-transparent justify-center items-center mt-5 px-6 lg:px-10 py-1 mx-auto relative">
         {/* <div className="flex block items-center space-x-4">
           <img src="/images/resoundifyLogo1.png" alt="Resoundify Logo" className="h-14 w-auto object-cover rounded-lg" />
         </div> */}
         <div></div>
 
-        <nav className="flex items-center space-x-8 font-sans text-lg text-black hidden md:flex">
+        <nav className="flex items-center space-x-20 font-medium text-xl text-black hidden md:flex">
           <Link to="/" className="relative hover:text-white transition duration-300 after:absolute after:bottom-0 after:left-0 after:w-0 after:h-0.5 after:bg-white after:transition-all after:duration-300 hover:after:w-full">
             Home
           </Link>
