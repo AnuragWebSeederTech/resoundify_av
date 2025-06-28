6339d7c2d808aec089c1e4377e2ffaf4c21e02c1
AnuragWebSeederTech <srivastava.anurag@webseeder.tech>
2025-06-28 13:36:58 +0530

    changed bg for about

--- src/components/aboutComponents/AboutHero.jsx
+++ src/components/aboutComponents/AboutHero.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 
 const AboutHero = () => {
   return (
-    <section className="relative h-[65vh] bg-cover bg-top" style={{ backgroundImage: "url('/images/aboutHero.png')" }}>
+    <section className="relative h-[65vh] bg-cover bg-top">
       {/* Dark Overlay */}
-      <div className="absolute inset-0 bg-black opacity-40"></div>
+      <div className="absolute inset-0 bg-gradient-to-r from-blue-300 via-black to-blue-300"></div>
 
       <div className="relative z-10 flex items-center justify-center h-full">
         <h1 className="text-5xl md:text-6xl lg:text-7xl font-medium text-white mb-6 leading-tight tracking-tight animate-fade-in-up delay-[200ms] drop-shadow-[0_0_8px_rgba(0,0,0,0.8)] font-[Tilt_Neon]">
